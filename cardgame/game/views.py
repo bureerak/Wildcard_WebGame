372135036bc05e130fe3d7634565ea321786b8d2
bureerak <pongphon_brr@hotmail.com>
2024-09-22 17:38:11 +0700

    Add Workspace
    add main.html and link between lobby.html

--- cardgame/game/views.py
+++ cardgame/game/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def in_game(request):
+    return render(request, 'board/lobby.html')
+
+def home_page(request):
+    return render(request, 'home/main.html')
