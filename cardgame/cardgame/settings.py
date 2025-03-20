1b75bc80d9ad42b0bf23205fc953f48f08dfea33
bureerak <pongphon_brr@hotmail.com>
2025-03-20 12:21:53 +0700

    now

--- cardgame/cardgame/settings.py
+++ cardgame/cardgame/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'game', 'static'),  # ที่ตั้งไฟล์ static ของแอป game
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
