27cc531e9216ddba4a1175b95664dcf5e46b5f07
bureerak <pongphon_brr@hotmail.com>
2025-03-20 12:00:13 +0700

    fixed

--- cardgame/cardgame/asgi.py
+++ cardgame/cardgame/asgi.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from game.rounting import wsPattern
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cardgame.settings')
+django.setup()
 
 http_response_app = get_asgi_application()
 
--- cardgame/cardgame/wsgi.py
+++ cardgame/cardgame/wsgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cardgame.settings')
+django.setup()
 
 application = get_wsgi_application()
